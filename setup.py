8b4eaa49c8b72f1f213b2b1119bf4fa8adec9e87
Colin Alston <colin.alston@gmail.com>
2014-01-26 13:35:49 +0200

    Add a setup.py and Travis scripts

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+
+def listify(filename):
+    return filter(None, open(filename, 'r').read().strip('\n').split('\n'))
+
+setup(
+    name="distributex",
+    version="0.1j",
+    url='http://github.com/calston/distributex',
+    license='MIT',
+    description="Distributex. A network mutex service for distributed"
+                "environments.",
+    long_description=open('README.md', 'r').read(),
+    author='Colin Alston',
+    author_email='colin.alston@gmail.com',
+    packages=[
+        "distributex",
+        "twisted.plugins",
+    ],
+    package_data={
+        'twisted.plugins': ['twisted/plugins/distributex_plugin.py']
+    },
+    include_package_data=True,
+    install_requires=listify('requirements.txt'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: System :: Clustering',
+        'Topic :: System :: Distributed Computing',
+    ],
+)
