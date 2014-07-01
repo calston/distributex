81c594bfcfed1a2bf5fd29affbfb3f91744876cd
Colin Alston <colin.alston@gmail.com>
2014-07-01 16:18:07 +0200

    Increase version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def listify(filename):
 
 setup(
     name="distributex",
-    version="0.2",
+    version="0.5",
     url='http://github.com/calston/distributex',
     license='MIT',
     description="A network mutex service for distributed environments.",
